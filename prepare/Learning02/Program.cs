ecffa9d1bec078fd5730ed53d304a6b227d56e45
awbingham <awbingham@hotmail.com>
2023-05-16 12:01:09 -0600

    Added Job

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+       job1._jobTitle = "Technical Support Analyst";
+       job1._company = "DealerSocket";
+       job1._startYear = 2016;
+       job1._endYear = 2021;
+
+       Job job2 = new Job();
+       job2._jobTitle = "Technical Support Analyst";
+       job2._company = "ICE Mortage Technology";
+       job2._startYear = 2021;
+       job2._endYear = 2023;
     }
 }
