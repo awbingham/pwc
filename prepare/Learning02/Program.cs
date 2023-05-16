2fb91d8692e3c91b207683029be872bec1c0a975
awbingham <awbingham@hotmail.com>
2023-05-16 12:16:53 -0600

    Added Resume Class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
        Job job1 = new Job();
-       job1._jobTitle = "Technical Support Analyst";
+       job1._jobTitle = "Technical Support Engineer";
        job1._company = "DealerSocket";
        job1._startYear = 2016;
        job1._endYear = 2021;
@@ -15,5 +15,11 @@ class Program
        job2._company = "ICE Mortage Technology";
        job2._startYear = 2021;
        job2._endYear = 2023;
+
+       Resume myResume = new Resume();
+       myResume._name = "Andrew Bingham";
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+       myResume.Display();
     }
 }
